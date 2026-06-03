07a6ad757b0facedc8b7a2a5a5374955d3cb5ec3
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2026-06-04 00:30:24 +0200

    Add iOS simulator artifacts

---
+++ Heroes3MapReader.iOS/Main.cs
@@ -0,0 +1,11 @@
+using UIKit;
+
+namespace Heroes3MapReader.iOS;
+
+public static class MainClass
+{
+    private static void Main(string[] args)
+    {
+        UIApplication.Main(args, null, typeof(AppDelegate));
+    }
+}
